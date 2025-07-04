300ccd86ea2b9b12d34c54954e2705aa8117f4f9
fernan0502 <142690777+fernan0502@users.noreply.github.com>
2025-07-04 07:34:17 -0500

    Update Proyecto_EDD.java

--- Proyecto_EDD.java
+++ Proyecto_EDD.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+package Grupo_12;
 
-package com.mycompany.proyecto;
-
-/**
- *
- * @author kiara
- */
 import java.util.Scanner;
 
-public class Proyecto_EDD {
+public class Grupo_12 {
     public static void main(String[] args) {
 
         GestorContactos gestor = new GestorContactos();
