c895e1582cc514f9c9f5e50a911088f6487de6dc
Kcmoranj <138317440+Kcmoranj@users.noreply.github.com>
2025-07-04 23:47:05 -0500

    Update ListaCircularDoble.java

--- ListaCircularDoble.java
+++ ListaCircularDoble.java
@@ -17,7 +17,7 @@ public class ListaCircularDoble<T> implements Iterable<T> {
     }
     private Nodo cabeza = null;
     private int tamaño = 0;
-
+    
     public void agregarAlFinal(T dato) {
         Nodo nuevo = new Nodo(dato);
         if (cabeza == null) {
@@ -33,7 +33,7 @@ public class ListaCircularDoble<T> implements Iterable<T> {
         }
         tamaño++;
     }
-
+    
     public T obtener(int indice) {
         if (indice < 0 || indice >= tamaño) {
             throw new IndexOutOfBoundsException("Índice inválido");
@@ -44,15 +44,12 @@ public class ListaCircularDoble<T> implements Iterable<T> {
         }
         return actual.dato;
     }
-
     public int tamaño() {
         return tamaño;
     }
-
     public boolean estaVacia() {
         return tamaño == 0;
     }
-
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
@@ -74,54 +71,49 @@ public class ListaCircularDoble<T> implements Iterable<T> {
             }
         };
     }
-    public void eliminar(int indice) {
-    if (indice < 0 || indice >= tamaño) {
-        throw new IndexOutOfBoundsException("Índice inválido");
-    }
-
-    Nodo actual = cabeza;
-
-    for (int i = 0; i < indice; i++) {
-        actual = actual.siguiente;
-    }
-
-    if (tamaño == 1) {
-        cabeza = null;
-    } else {
-        actual.anterior.siguiente = actual.siguiente;
-        actual.siguiente.anterior = actual.anterior;
-
-        if (actual == cabeza) {
-            cabeza = actual.siguiente;
-        }
-    }
-
-    tamaño--;
-}
-    public void clear() {
-    cabeza = null;
-    tamaño = 0;
-}
-    public void ordenar(Comparator<T> comparador) {
-    if (tamaño <= 1) return; // nada que ordenar
     
-    boolean cambiado;
-    do {
-        cambiado = false;
+    public void eliminar(int indice) {
+        if (indice < 0 || indice >= tamaño) {
+            throw new IndexOutOfBoundsException("Índice inválido");
+        }
         Nodo actual = cabeza;
-        for (int i = 0; i < tamaño - 1; i++) {
-            Nodo siguiente = actual.siguiente;
-            if (comparador.compare(actual.dato, siguiente.dato) > 0) {
-                // Intercambiar datos (no nodos)
-                T temp = actual.dato;
-                actual.dato = siguiente.dato;
-                siguiente.dato = temp;
-                cambiado = true;
-            }
+        for (int i = 0; i < indice; i++) {
             actual = actual.siguiente;
         }
-    } while (cambiado);
-}
 
+        if (tamaño == 1) {
+            cabeza = null;
+        } else {
+            actual.anterior.siguiente = actual.siguiente;
+            actual.siguiente.anterior = actual.anterior;
 
+            if (actual == cabeza) {
+                cabeza = actual.siguiente;
+            }
+        }
+        tamaño--;
+    }
+    public void clear() {
+        cabeza = null;
+        tamaño = 0;
+    }
+    public void ordenar(Comparator<T> comparador) {
+        if (tamaño <= 1) return; // nada que ordenar
+        boolean cambiado;
+        do {
+            cambiado = false;
+            Nodo actual = cabeza;
+            for (int i = 0; i < tamaño - 1; i++) {
+                Nodo siguiente = actual.siguiente;
+                if (comparador.compare(actual.dato, siguiente.dato) > 0) {
+                    // Intercambiar datos (no nodos)
+                    T temp = actual.dato;
+                    actual.dato = siguiente.dato;
+                    siguiente.dato = temp;
+                    cambiado = true;
+                }
+                actual = actual.siguiente;
+            }
+        } while (cambiado);
+    }
 }
